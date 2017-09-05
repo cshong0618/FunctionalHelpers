28f43a7e652950b9dfba7ed85aa4ee1adfc01cf8
Chong Shao Hong <cshong.gcc@hotmail.com>
2017-09-06 02:55:02 +0800

    Added isPrime
    Refactored methods into classes

--- src/shaong/FunctionalHelper.java
+++ src/shaong/FunctionalHelper.java
@@ -1,5 +1,7 @@
 package shaong;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by shaong on 9/6/17.
  */
@@ -7,8 +9,42 @@ package shaong;
 public class FunctionalHelper {
     private FunctionalHelper(){}
 
-    public static int doubleToInt(double d) {return (int) d;}
-    public static long doubleToLong(double d) {return (long) d;}
-    public static double intToDouble(int i) {return (double) i;}
-    public static double longToDouble(long l) {return (double) l;}
+    public static class Conversion {
+        public static int doubleToInt(double d) {
+            return (int) d;
+        }
+
+        public static long doubleToLong(double d) {
+            return (long) d;
+        }
+
+        public static double intToDouble(int i) {
+            return (double) i;
+        }
+
+        public static double longToDouble(long l) {
+            return (double) l;
+        }
+    }
+
+    public static class Numbers {
+        // Even checks
+        public static boolean isEven(int i) {
+            return (i & 1) == 0;
+        }
+
+        public static boolean isEven(long l) {
+            return (l & 1) == 0;
+        }
+
+        // Prime checks
+        public static boolean isPrime(int i) {
+            if(i < 2) return false;
+            if(i == 2) return true;
+            return IntStream.rangeClosed(2, (int)Math.sqrt((double)i))
+                    .boxed()
+                    .filter(e -> i % e == 0)
+                    .count() == 0;
+        }
+    }
 }
--- test/FunctionalHelperTest.java
+++ test/FunctionalHelperTest.java
@@ -13,7 +13,7 @@ public class FunctionalHelperTest {
         List<Double> init = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
         List<Integer> expected = IntStream.rangeClosed(1, 5).boxed().collect(Collectors.toList());
         List<Integer> result = init.stream()
-                .map(FunctionalHelper::doubleToInt)
+                .map(FunctionalHelper.Conversion::doubleToInt)
                 .collect(Collectors.toList());
 
         Assert.assertEquals(expected, result);
@@ -21,6 +21,18 @@ public class FunctionalHelperTest {
 
     @Test
     public void doubleToLong() throws Exception {
+    }
+
+    @Test
+    public void isPrimeTest() {
+        List<Integer> numbers = IntStream.rangeClosed(1, 20).boxed().collect(Collectors.toList());
+        List<Integer> primes = numbers.stream()
+                .filter(FunctionalHelper.Numbers::isPrime)
+                .collect(Collectors.toList());
+
+        List<Integer> expected = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19);
+
+        Assert.assertEquals(expected, primes);
     }
 
 }
